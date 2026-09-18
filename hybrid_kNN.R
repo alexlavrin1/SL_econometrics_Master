0536865762580acda53ab2ba7a4a3fac0c04255a
Ensar-Tasgin <ensartasgin@gmail.com>
2026-09-18 15:45:46 +0200

    Add files via upload

---
+++ hybrid_kNN.R
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------
+# Author:
+# Group 6 Alexandre Lavrinenko,Ensar Tasgin 646820
+# Sanne Maasman Sebastiaan van Helden
+# --------------------------------------------------------------------
+
+## Use this code skeleton to implement the hybrid kNN algorithm.  Please submit 
+## your implementation together with your report and your simulation code.
+
+## IMPORTANT: Please do not change the function name and make sure that you
+##            use the correct input and output as specified below. This will 
+##            simplify grading because each group's code will be similarly 
+##            structured.  However, feel free to add other arguments to the 
+##            function definition as needed.
+
+
+# Input:
+# X ... data matrix
+# k ... number of neighbors
+# add any other arguments you need
+
+# Output
+# A list with the following components:
+# X_hat ....... the imputed data set
+# weights ..... a list of variable importance weights from the random forest
+#               models. That is, each list element contains the weights for 
+#               the corresponding variable to be imputed. Use the variable name 
+#               as the name of the list element.
+# add any other output you want to return as additional components
+
+hybrid_kNN <- function(X, k, ...) {
+  
+  # *enter your code here*
+  
+}
